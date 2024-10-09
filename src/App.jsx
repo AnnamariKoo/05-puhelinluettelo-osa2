4c785c34551f82808a9e83acbab1f705a13dd547
AnnamariKoo <163762866+AnnamariKoo@users.noreply.github.com>
2024-10-09 17:59:01 +0300

    Luetteloon lisättävät uudet numerot synkronoidaan palvelimelle

--- src/App.jsx
+++ src/App.jsx
@@ -31,10 +31,14 @@ const App = () => {
       alert(`${newName} is already added to phonebook`);
       return;
     }
-
-    setPersons(persons.concat(personObject));
-    setNewName("");
-    setNewNumber("");
+    axios
+      .post("http://localhost:3001/persons", personObject)
+      .then((response) => {
+        console.log(response);
+        setPersons(persons.concat(response.data));
+        setNewName("");
+        setNewNumber("");
+      });
   };
 
   const handleSearchChange = (event) => {
